f1b0ca4e61c4eb4e325f2b4c467ee5356880b8bd
Sydney <hribar.sydney@gmail.com>
2022-10-03 08:07:16 -0400

    Add print statements to welcome visitors

--- OperationFury.py
+++ OperationFury.py
@@ -6,3 +6,7 @@
 Our Welcome Screen will start our program letting
 drivers know that the InfoTechCenter OS is loading
 """
+
+print("\n\nWelcome to Operation Fury InfoTech Center")
+
+print("\nOperation Fury's Operating System is Booting up")
