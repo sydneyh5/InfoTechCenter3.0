a61244b637617361fca59ad47fa6e46efa400794
Sydney <hribar.sydney@gmail.com>
2022-10-05 08:39:20 -0400

    Initial Weather Commit

---
+++ OperationFury.py
@@ -0,0 +1,10 @@
+#Weather
+#Developer
+#Version 1.0
+
+"""
+Create a function for our current weather using the
+ramdom.choice function to determine what the weather
+is picking from a list - using If, elif, and else statements
+to check the condition and print a specific print line
+"""
