6c4555b52bcb31613ef997c07aca4010ef292ada
Sydney <hribar.sydney@gmail.com>
2022-10-05 08:06:16 -0400

    Add For Loop "OS Booting Up"

--- OperationFury.py
+++ OperationFury.py
@@ -12,8 +12,13 @@ from time import sleep #We imported the Sleep function from the Time library
 
 #To change the color of the text out put \033[1;32m and change the last number to change
 #The text color
+#To reset the color use \033[0m
 
-print("\n\033[1;36m Welcome to Operation Fury InfoTech Center")
+
+print("\n\033[1;36mWelcome to Operation Fury InfoTech Center")
 sleep(2)
-print("\nOperation Fury's Operating System is Booting up")
+print("\n\033[1;34mOperation Fury's Operating System is Booting up\033[0m")
 
+for i in range(3):
+    print("OS Booting up")
+    sleep(1.25)
