98b8f127db87f67fd24926e5f8a0edd4f7788976
Sydney <hribar.sydney@gmail.com>
2022-10-10 07:59:06 -0400

    Added elif and else statements - Check weather conditions

--- OperationFury.py
+++ OperationFury.py
@@ -15,7 +15,7 @@ import random
 #Weather Condition list using the random.choice library
 #to randomly choose a condition and storing it in it's brain
 def weather():
-    weatherForcast = ["Rain", "Snow", "Sunny", "Cloudy", "Foggy", "Storming", "Icy"]
+    weatherForcast = ["Rain", "Snow", "Sunny", "Windy", "Foggy", "Storming", "Icy"]
     weatherCondition = random.choice(weatherForcast)
     return weatherCondition
 
@@ -24,6 +24,25 @@ weatherAlert = weather()
 def vehicleResponseSystem():
     if weatherAlert == "Icy":
         print("\nVRS has changed your Alarm 30 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 30MPH")
+    elif weatherAlert == "Snow":
+        print("\nVRS has changed your Alarm 15 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 50MPH")
+    elif weatherAlert == "Rain":
+        print("\nVRS has changed your Alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 60MPH")
+    elif weatherAlert == "Windy":
+        print("\nVRS has changed your Alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 70MPH")
+    elif weatherAlert == "Foggy":
+        print("\nVRS has changed your Alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 65MPH")
+    elif weatherAlert == "Storming":
+        print("\nVRS has changed your Alarm 15 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 50MPH")
+    else:
+        print("\nThe weather today is",weatherAlert)
+        print("VRS will allow your car to go 95MPH")
 
 
 vehicleResponseSystem()
