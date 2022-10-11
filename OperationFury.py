dcabb36735d9e7c1ecac8412226c61bf6afc7a69
Sydney <hribar.sydney@gmail.com>
2022-10-11 08:23:12 -0400

    Define Gas Level Function

--- OperationFury.py
+++ OperationFury.py
@@ -9,6 +9,7 @@ we have until we need gasoline based on an if, elif, else condition
 
 #Import library here
 import random
+from time import sleep
 
 #Gas Level Function
 def gasLevelGauge():
@@ -16,4 +17,13 @@ def gasLevelGauge():
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
-gasLevelGauge()
+#Variable calling the gasLevelGauge function to store value once
+gasLevelIndicator = gasLevelGauge()
+
+def gasLevelAlert():
+    if gasLevelIndicator == "Empty":
+        print("\033[1;31m***WARNING YOU ARE ON EMPTY***\033[0m")
+        sleep(1.25)
+        print("Calling Emergency Contact")
+
+gasLevelAlert()
